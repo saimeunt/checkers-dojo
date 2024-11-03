f1df750b2698a19e9452eb231d9a520284876b0e
Nadai2010 <nadai2010@proton.me>
2024-11-03 15:37:32 +0000

    UI old changes

--- client/src/bindings.ts
+++ client/src/bindings.ts
@@ -1,132 +1,71 @@
-import type { SchemaType } from "@dojoengine/sdk";
-
-// Type definition for `dojo_starter::models::PieceValue` struct
-export interface PieceValue {
-	fieldOrder: string[];
-	position: Position;
-	is_king: boolean;
-	is_alive: boolean;
+interface Moves {
+    fieldOrder: string[];
+    player: string;
+    remaining: number;
+    last_direction: Direction;
+    can_move: boolean;
 }
 
-// Type definition for `dojo_starter::models::Coordinates` struct
-export interface Coordinates {
-	fieldOrder: string[];
-	raw: number;
-	col: number;
+interface DirectionsAvailable {
+    fieldOrder: string[];
+    player: string;
+    directions: Direction[];
 }
 
-// Type definition for `dojo_starter::models::Piece` struct
-export interface Piece {
-	fieldOrder: string[];
-	player: string;
-	coordinates: Coordinates;
-	position: Position;
-	is_king: boolean;
-	is_alive: boolean;
+interface Position {
+    fieldOrder: string[];
+    player: string;
+    vec: Vec2;
 }
 
-// Type definition for `dojo_starter::models::Position` enum
-export enum Position {
-	None,
-	Up,
-	Down,
+enum Direction {
+    None = "0",
+    UpLeft = "1",
+    UpRight = "2",
+    DownLeft = "3",
+    DownRight = "4",
 }
 
-export interface DojoStarterSchemaType extends SchemaType {
-	dojo_starter: {
-		PieceValue: PieceValue,
-		Coordinates: Coordinates,
-		Piece: Piece,
-		ERC__Balance: ERC__Balance,
-		ERC__Token: ERC__Token,
-		ERC__Transfer: ERC__Transfer,
-	},
+interface Vec2 {
+    x: number;
+    y: number;
 }
-export const schema: DojoStarterSchemaType = {
-	dojo_starter: {
-		PieceValue: {
-			fieldOrder: ['position', 'is_king', 'is_alive'],
-			position: Position.None,
-			is_king: false,
-			is_alive: false,
-		},
-		Coordinates: {
-			fieldOrder: ['raw', 'col'],
-			raw: 0,
-			col: 0,
-		},
-		Piece: {
-			fieldOrder: ['player', 'coordinates', 'position', 'is_king', 'is_alive'],
-			player: "",
-			coordinates: { fieldOrder: ['raw', 'col'], raw: 0, col: 0, },
-			position: Position.None,
-			is_king: false,
-			is_alive: false,
-		},
-		ERC__Balance: {
-			fieldOrder: ['balance', 'type', 'tokenmetadata'],
-			balance: '',
-			type: 'ERC20',
-			tokenMetadata: {
-				fieldOrder: ['name', 'symbol', 'tokenId', 'decimals', 'contractAddress'],
-				name: '',
-				symbol: '',
-				tokenId: '',
-				decimals: '',
-				contractAddress: '',
-			},
-		},
-		ERC__Token: {
-			fieldOrder: ['name', 'symbol', 'tokenId', 'decimals', 'contractAddress'],
-			name: '',
-			symbol: '',
-			tokenId: '',
-			decimals: '',
-			contractAddress: '',
-		},
-		ERC__Transfer: {
-			fieldOrder: ['from', 'to', 'amount', 'type', 'executed', 'tokenMetadata'],
-			from: '',
-			to: '',
-			amount: '',
-			type: 'ERC20',
-			executedAt: '',
-			tokenMetadata: {
-				fieldOrder: ['name', 'symbol', 'tokenId', 'decimals', 'contractAddress'],
-				name: '',
-				symbol: '',
-				tokenId: '',
-				decimals: '',
-				contractAddress: '',
-			},
-			transactionHash: '',
-		},
 
-	},
+type Schema = {
+    dojo_starter: {
+        Moves: Moves;
+        DirectionsAvailable: DirectionsAvailable;
+        Position: Position;
+    };
 };
-// Type definition for ERC__Balance struct
-export type ERC__Type = 'ERC20' | 'ERC721';
-export interface ERC__Balance {
-    fieldOrder: string[];
-    balance: string;
-    type: string;
-    tokenMetadata: ERC__Token;
+
+enum Models {
+    Moves = "dojo_starter-Moves",
+    DirectionsAvailable = "dojo_starter-DirectionsAvailable",
+    Position = "dojo_starter-Position",
 }
-export interface ERC__Token {
-    fieldOrder: string[];
-    name: string;
-    symbol: string;
-    tokenId: string;
-    decimals: string;
-    contractAddress: string;
-}
-export interface ERC__Transfer {
-    fieldOrder: string[];
-    from: string;
-    to: string;
-    amount: string;
-    type: string;
-    executedAt: string;
-    tokenMetadata: ERC__Token;
-    transactionHash: string;
-}
+
+const schema: Schema = {
+    dojo_starter: {
+        Moves: {
+            fieldOrder: ["player", "remaining", "last_direction", "can_move"],
+            player: "",
+            remaining: 0,
+            last_direction: Direction.None,
+            can_move: false,
+        },
+        DirectionsAvailable: {
+            fieldOrder: ["player", "directions"],
+            player: "",
+            directions: [],
+        },
+        Position: {
+            fieldOrder: ["player", "vec"],
+            player: "",
+            vec: { x: 0, y: 0 },
+        },
+    },
+};
+
+export type { Schema, Moves, DirectionsAvailable, Position, Vec2 };
+export { Direction, schema, Models };
--- client/src/hooks/useDojoConnect.tsx
+++ client/src/hooks/useDojoConnect.tsx
@@ -1,25 +1,86 @@
-import { useEffect, } from "react";
+import { useEffect, useMemo } from "react";
 import { SDK, createDojoStore } from "@dojoengine/sdk";
+import { getEntityIdFromKeys } from "@dojoengine/utils";
 import { addAddressPadding } from "starknet";
-import { schema } from "../bindings.ts";
+import { Models, Schema } from "../bindings.ts";
 import { useDojo } from "./useDojo.tsx";
+import useModel from "./useModel.tsx";
 import { useSystemCalls } from "./useSystemCalls.ts";
 
-export const useDojoStore = createDojoStore<typeof schema>();
+export const useDojoStore = createDojoStore<Schema>();
 
-function useDojoConnect({ sdk }: { sdk: SDK<typeof schema> }) {
+function useDojoConnect({ sdk }: { sdk: SDK<Schema> }) {
   const { account } = useDojo();
+  
+
   const state = useDojoStore((state) => state);
   const entities = useDojoStore((state) => state.entities);
+  
+
   const { spawn } = useSystemCalls();
+
+  const entityId = useMemo(
+    () => getEntityIdFromKeys([BigInt(account?.account.address)]),
+    [account?.account.address]
+  );
+
+  useEffect(() => {
+    let unsubscribe: (() => void) | undefined;
+
+    const subscribe = async () => {
+      try {
+        const subscription = await sdk.subscribeEntityQuery(
+          {
+            dojo_starter: {
+              Moves: {
+                $: {
+                  where: {
+                    player: { $is: addAddressPadding(account.account.address) },
+                  },
+                },
+              },
+              Position: {
+                $: {
+                  where: {
+                    player: { $is: addAddressPadding(account.account.address) },
+                  },
+                },
+              },
+            },
+          },
+          (response) => {
+            if (response.error) {
+              console.error("Error setting up entity sync:", response.error);
+            } else if (response.data && response.data[0].entityId !== "0x0") {
+              console.log("Subscribed:", response.data[0]);
+              state.updateEntity(response.data[0]);
+            }
+          },
+          { logging: true }
+        );
+
+        unsubscribe = () => subscription.cancel();
+      } catch (error) {
+        console.error("Subscription error:", error);
+      }
+    };
+
+    subscribe();
+
+    return () => {
+      if (unsubscribe) {
+        unsubscribe();
+      }
+    };
+  }, [sdk, account?.account.address, state]);
 
   useEffect(() => {
     const fetchEntities = async () => {
       try {
-        await sdk.client.getEntities(
+        await sdk.getEntities(
           {
             dojo_starter: {
-              Piece: {
+              Moves: {
                 $: {
                   where: { player: { $eq: addAddressPadding(account.account.address) } },
                 },
@@ -44,6 +105,10 @@ function useDojoConnect({ sdk }: { sdk: SDK<typeof schema> }) {
     fetchEntities();
   }, [sdk, account?.account.address, state]);
 
-  return { spawn, account, entities };
+  const moves = useModel(entityId, Models.Moves);
+  const position = useModel(entityId, Models.Position);
+
+  return { spawn, account, moves, position, entities };
 }
+
 export default useDojoConnect;
